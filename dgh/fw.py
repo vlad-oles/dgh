775ea4cb6c7cba7f6f7dd1a11c66011a2b49a613
vlad-oles <vlad.oles@protonmail.com>
2025-03-12 22:23:10 -0700

    refactored to remove redundancy

--- dgh/fw.py
+++ dgh/fw.py
@@ -5,7 +5,7 @@ import jax
 from .mappings import is_row_stoch, fg_to_R
 from .spaces import arrange_distances
 
-def solve_frank_wolfe(obj, grad, find_descent_direction, minimize_obj_wrt_gamma, S0,
+def solve_frank_wolfe(obj, grad, find_descent_direction, minimize_obj_wrt_alpha, S0,
                       tol=1e-16, max_iter=np.inf, verbose=0):
     """
     Minimizes smoothed distortion σ over the bi-mapping polytope 𝓢.
@@ -13,7 +13,7 @@ def solve_frank_wolfe(obj, grad, find_descent_direction, minimize_obj_wrt_gamma,
     :param obj: smoothed distortion σ:𝓢🠒ℝ (function)
     :param grad: ∇σ:𝓢🠒𝓢 (function)
     :param find_descent_direction: R:ℝ^(n+m)×(n+m)🠒𝓢 (function)
-    :param minimize_obj_wrt_gamma: γ*:𝓢×𝓢🠒ℝ (function)
+    :param minimize_obj_wrt_alpha: α*:𝓢×𝓢🠒ℝ (function)
     :param S0: starting point in 𝓢 (2d-array)
     :param tol: tolerance for measuring rate of descent (float)
     :param max_iter: maximum number of iterations (int or ∞)
@@ -27,21 +27,19 @@ def solve_frank_wolfe(obj, grad, find_descent_direction, minimize_obj_wrt_gamma,
         R = find_descent_direction(grad_at_S)
         D = R - S
 
-        # Find γ ∈ [0, 1] defining how much to go in the decided direction.
-        global_gamma = minimize_obj_wrt_gamma(S, D)
-        critical_gammas = {0, 1}
-        if 0 < global_gamma < 1:
-            critical_gammas.add(global_gamma)
-        gamma = min(critical_gammas, key=lambda x: obj(S + x*D))
+        # Find α ∈ [0, 1] defining how much to go in the decided direction.
+        global_alpha = minimize_obj_wrt_alpha(S, D)
+        critical_alphas = {0, 1, global_alpha}
+        alpha = min(critical_alphas, key=lambda x: obj(S + x*D))
 
         if verbose > 2:
-            print(f'  iter {iter}: σ(S)={obj(S):.4f}, γ={gamma:.5f}')
+            print(f'  iter {iter}: σ(S)={obj(S):.4f}, α={alpha:.5f}')
 
-        # Move S towards R by γ, i.e. to (1-γ)S + γR.
-        S += gamma * D
+        # Move S towards R by α, i.e. to (1-α)S + αR.
+        S += alpha * D
 
         # Stop if the rate of descent is too small or if the line search stalls.
-        if np.sum(-grad_at_S * D) < tol or np.isclose(gamma, 0):
+        if np.sum(-grad_at_S * D) < tol or np.isclose(alpha, 0):
             break
 
     return S, iter + 1
@@ -61,21 +59,20 @@ def make_frank_wolfe_solver(X, Y, c, **kwargs):
     n, m = len(X), len(Y)
     X__Y, Y__X, _Y_X = arrange_distances(X, Y)
 
-    # precompute the exponentials
-    c_Y_X, c__Y_X = c**_Y_X, c**-_Y_X
-    c_X__Y, c__X__Y = c**X__Y, c**-X__Y
-    c_Y__X, c__Y__X = c**Y__X, c**-Y__X
+    # Precompute the exponentials.
+    exp__Y_X, exp_neg__Y_X = c**_Y_X, c**-_Y_X
+    exp_X__Y, exp_neg_X__Y = c**X__Y, c**-X__Y
+    exp_Y__X, exp_neg_Y__X = c**Y__X, c**-Y__X
 
     @jax.jit
-    def jax_matrix_multiply(c_Y_X, c__Y_X, c_X__Y, c__X__Y, c_Y__X, c__Y__X, S):
-        return (c__Y_X @ S @ c_Y_X + c_Y_X @ S @ c__Y_X).T + \
-           c__X__Y @ S @ c_Y__X + c_X__Y @ S @ c__Y__X
+    def jax_matrix_multiply(S):
+        return (exp_neg__Y_X @ S @ exp__Y_X + exp__Y_X @ S @ exp_neg__Y_X).T + \
+           exp_neg_X__Y @ S @ exp_Y__X + exp_X__Y @ S @ exp_neg_Y__X
     
     # Define auxiliary function that is a component in the objective and its gradient.
     def dot_multiplicand(S):
-
-        # use precomputed values and precompiled jax function instead.
-        return jax_matrix_multiply(c_Y_X, c__Y_X, c_X__Y, c__X__Y, c_Y__X, c__Y__X, S)
+        # Use precomputed values and precompiled jax function.
+        return jax_matrix_multiply(S)
 
     # Smooth distortion σ as the objective.
     def obj(S):
@@ -92,17 +89,17 @@ def make_frank_wolfe_solver(X, Y, c, **kwargs):
 
         return fg_to_R(f, g)
 
-    # To minimize σ(γ) = σ(S + γD), for line search.
-    def minimize_obj_wrt_gamma(S, D):
-        # Leverage that the objective is quadratic in γ, σ(γ) = aγ² + bγ + c.
+    # To minimize σ(α) = σ(S + αD), for line search.
+    def minimize_obj_wrt_alpha(S, D):
+        # Leverage that the objective is quadratic in α, σ(α) = aα² + bα + c.
         a = np.sum(D * dot_multiplicand(D))
         b = np.sum(D * dot_multiplicand(S)) + np.sum(S * dot_multiplicand(D))
         with np.errstate(divide='ignore', invalid='ignore'):
-            global_gamma = np.divide(-b, 2*a)
+            global_alpha = np.divide(-b, 2*a)
 
-        return global_gamma
+        return global_alpha
 
     fw = partial(solve_frank_wolfe, obj, grad, find_descent_direction,
-                 minimize_obj_wrt_gamma, **kwargs)
+                 minimize_obj_wrt_alpha, **kwargs)
 
     return fw
